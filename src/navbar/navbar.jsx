24f27ce0d937d8b3df55539b24fb7ac71621961a
YoselinMejia <20011412@galileo.edu>
2024-10-01 04:39:01 -0600

    ahora es responsive

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -5,17 +5,20 @@ import './navcss.css';
 
 const NavigationBar = () => {
   return (
-    <Navbar  variant="dark">
-       <Navbar.Brand as={Link} to="/">Inicio</Navbar.Brand>
-       
-      <Nav className="me-auto">
-        <Nav.Link as={Link} to="/crearCuenta">Crear Cuenta</Nav.Link>
-        <Nav.Link as={Link} to="/catalogo">Catálogo</Nav.Link>
-        <Nav.Link as={Link} to="/courier">Courier</Nav.Link>
-        <Nav.Link as={Link} to="/inventario">Inventario</Nav.Link>
-        <Nav.Link as={Link} to="/payment">Pago</Nav.Link>
-        <Nav.Link as={Link} to="/Admin">Admin</Nav.Link>
-      </Nav>
+    <Navbar variant="dark" expand="lg"> 
+      <Navbar.Brand as={Link} to="/">Inicio</Navbar.Brand>
+      
+      <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/crearCuenta">Crear Cuenta</Nav.Link>
+          <Nav.Link as={Link} to="/catalogo">Catálogo</Nav.Link>
+          <Nav.Link as={Link} to="/courier">Courier</Nav.Link>
+          <Nav.Link as={Link} to="/inventario">Inventario</Nav.Link>
+          <Nav.Link as={Link} to="/payment">Pago</Nav.Link>
+          <Nav.Link as={Link} to="/Admin">Admin</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
